3de4edac938dff07251225bdd7b4f25e1c9e2373
LavanyaB1605 <lavanya.bhaskaran16@gmail.com>
2022-11-14 21:01:54 +0530

    Added Last Name Validation with Lambda Expression

--- src/UserRegistration_Lambda.java
+++ src/UserRegistration_Lambda.java
@@ -2,7 +2,7 @@ import java.util.regex.Pattern;
 
 @FunctionalInterface
 interface UserRegistrationInterface {
-    boolean validate(String details,String pattern);
+    boolean validate(String pattern, String details);
 }
 
 public class UserRegistration_Lambda {
@@ -18,9 +18,22 @@ public class UserRegistration_Lambda {
                 return false;
             }
         };
+        UserRegistrationInterface lastName = (String pattern, String last_name) -> {
+            if (Pattern.matches(pattern, last_name)) {
+                System.out.println("Entered Name is Valid");
+                return true;
+            }else {
+                System.out.println("Entered Name is Invalid");
+                return false;
+            }
+        };
 
         firstName.validate("^[A-Z][a-z]{2,}$", "Lavanya");
         firstName.validate("^[A-Z][a-z]{2,}$", "lavan");
+
+        lastName.validate("^[A-Z][a-z]{2,}$", "Bhaskaran");
+        //System.out.println("Entered value is " + lastName.validate("^[A-Z][a-z]{2,}$", "Bhaskaran"));
+        lastName.validate("^[A-Z][a-z]{2,}$", "bh");
 
     }
 }
